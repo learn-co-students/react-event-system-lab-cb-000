bbd03cdd5530b9821af04ba8842dad7e5c8125aa
Heather Christman <efl7a@yahoo.de>
2018-05-21 13:36:43 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component {
+  notFocused = () => {
+    console.log("Hey! Eyes on me!")
+  }
+  focused = () => {
+    console.log("Good!")
+  }
+
+  render() {
+    return(
+      <button onBlur={this.notFocused} onFocus={this.focused}>Do you see me?</button>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React from 'react';
+
+export default class Keypad extends React.Component {
+  inputting = () => {
+    console.log("Entering password...")
+  }
+  render() {
+    return(
+      <input type="password" onKeyUp={this.inputting}></input>
+    )
+  }
+}
